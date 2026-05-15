8f26d189dec586d82eff7de53f69cfcf3f61e96d
Dunaevsky, Alex <alex.dunaevsky@arcadis.com>
2026-05-15 15:51:19 -0400

    fix local build

--- webpack.config.js
+++ webpack.config.js
@@ -3,63 +3,67 @@ const { VueLoaderPlugin } = require('vue-loader');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  mode: process.env.NODE_ENV || 'development',
-  entry: './src/main.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/dev/multiStringPoker/1/',
-    clean: true,
-  },
-  resolve: {
-    extensions: ['.js', '.vue', '.json'],
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+module.exports = (env, argv = {}) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    mode: argv.mode || 'development',
+    entry: './src/main.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+      publicPath: isProduction ? '/dev/multiStringPoker/1/' : '/',
+      clean: true,
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
+    resolve: {
+      extensions: ['.js', '.vue', '.json'],
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
       },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        type: 'asset/resource',
-      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader',
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: 'babel-loader',
+        },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          type: 'asset/resource',
+        },
+      ],
+    },
+    plugins: [
+      new VueLoaderPlugin(),
+      new HtmlWebpackPlugin({
+        template: 'index.html',
+      }),
+      new MiniCssExtractPlugin(),
     ],
-  },
-  plugins: [
-    new VueLoaderPlugin(),
-    new HtmlWebpackPlugin({
-      template: 'index.html',
-    }),
-    new MiniCssExtractPlugin(),
-  ],
-  devServer: {
-    static: [
-      {
-        directory: path.join(__dirname, 'dist'),
-      },
-      {
-        directory: path.join(__dirname, 'static'),
-        publicPath: '/static',
-        watch: true,
-      },
-    ],
-    hot: true,
-    open: true,
-    port: 8081,
-    historyApiFallback: true,
-  },
+    devServer: {
+      static: [
+        {
+          directory: path.join(__dirname, 'dist'),
+        },
+        {
+          directory: path.join(__dirname, 'static'),
+          publicPath: '/static',
+          watch: true,
+        },
+      ],
+      hot: true,
+      open: true,
+      port: 8081,
+      historyApiFallback: true,
+    },
+  };
 };
